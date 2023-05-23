3e9b399ae21a0c780e5e88e9f809892b7b9b667c
sanghyunlee77 <22000516@handong.ac.kr>
2023-05-23 19:38:46 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+nclude <stdio.h>
+#include "mylib.h"
+
+int main(void){
+	int a = 3, b = 5;
+	printf("(intial) a=%d, b=%d\n", a, b);
+
+	swap(&a,&b);
+	printf("(swapped) a=%d, b=%d\n", a, b);
+
+	printf("(added) a + b = %d + %d = %d\n", a, b, add(a,b));
+
+	printf("(subtracted) a - b = %d - %d = %d\n", a, b, subtract(a,b));
+
+	return 0;
+}
---
+++ mylib.h
@@ -0,0 +1,8 @@
+#ifndef _MYLIB_H_
+#define _MYLIB_H_
+
+void swap(int*, int*);
+int add(int, int);
+int subtract(int, int);
+
+#endif
